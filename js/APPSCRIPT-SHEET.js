2227c78de5178c0016ce03f80f4667c6a1eb407f
De Oka <115537848+DeOkanee@users.noreply.github.com>
2024-07-18 14:40:22 +0800

    update code new

---
+++ js/APPSCRIPT-SHEET.js
@@ -0,0 +1,20 @@
+
+  //INPUTAN UNTUK PASEMAYA 1
+  function doPost(e) {
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Data1");
+    var newRow = sheet.getLastRow() + 1;
+    var rowData = [];
+  
+    rowData[0] = new Date().toLocaleString(); // Tanggal dan Waktu
+    rowData[1] = e.parameter.Name;            // Nama Lengkap
+    rowData[2] = e.parameter.Email;           // Email
+    rowData[3] = e.parameter.Jenjang;         // Jenjang
+    rowData[4] = e.parameter.Gender;          // Gender
+    rowData[5] = e.parameter.Asal_sekolah;           // Asal_sekolah
+    rowData[6] = e.parameter.Alamat;          // Alamat
+    rowData[7] = e.parameter.WhatsApp          //whatsapp
+  
+    sheet.getRange(newRow, 1, 1, rowData.length).setValues([rowData]);
+  
+    return ContentService.createTextOutput("");  // Mengembalikan teks kosong
+  }
--- js/view-img-list.js
+++ js/view-img-list.js
@@ -12,6 +12,35 @@ var imgCaption = document.getElementById("imgCaption");
 
 // Tambahkan event listener ke semua gambar dalam .lomba-list
 var images = document.querySelectorAll('.lomba-list img');
+
+images.forEach(function(image) {
+  image.addEventListener('click', function() {
+    imgModal.style.display = "block"; // Use flex to center image
+    modalImg.src = this.src;
+    imgCaption.innerHTML = this.nextElementSibling.querySelector('h2').innerHTML;
+  });
+});
+
+// Tambahkan event listener ke span untuk menutup modal
+imgClose.addEventListener('click', function() {
+  imgModal.style.display = "none";
+});
+
+
+// MENAMPILKAN PAMFLET
+// Dapatkan elemen modal
+var imgModal = document.getElementById("imgModal");
+
+// Dapatkan elemen gambar dalam modal
+var modalImg = document.getElementById("modalImg");
+
+// Dapatkan elemen span untuk menutup modal
+var imgClose = document.getElementsByClassName("img-close")[0];
+
+
+// Tambahkan event listener ke semua gambar dalam .lomba-list
+var images = document.querySelectorAll('.pamflet img');
+
 images.forEach(function(image) {
   image.addEventListener('click', function() {
     imgModal.style.display = "block"; // Use flex to center image
