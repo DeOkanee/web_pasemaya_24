45c1ea0324cad7a906beb2f7ad69e111acee6838
De Oka <115537848+DeOkanee@users.noreply.github.com>
2024-08-17 20:44:21 +0800

    update code new

--- js/pop up.js
+++ js/pop up.js
@@ -6,9 +6,9 @@ document.addEventListener('DOMContentLoaded', async function() {
         imageHeight: 200,
         imageAlt: 'Custom image',
         title: "Om Swastyastu",
-        html: "<b>PASEMAYA BEM FDA 2024</b></br><p>Berbagai lomba terbuka untuk semua! Daftarkan dirimu sekarang dan tunjukkan bakatmu!</p>",
-        footer: "Tinggal lomba Tari Puspanjali yang terbuka! Segera daftar sebelum terlambat!",
-        confirmButtonText: 'Daftar Sekarang',
+        html: "<b>PASEMAYA BEM FDA 2024</b></br><p>Semua lomba sudah ditutup dan tidak ada pendaftaran yang buka.</p>",
+        footer: "Terima kasih atas minat dan partisipasi Anda!",
+        confirmButtonText: 'Tutup',
     });
 
     setTimeout(mulai, 900); // Pastikan fungsi 'mulai' didefinisikan
